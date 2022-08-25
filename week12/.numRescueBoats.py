babdc83cd5ca21658e42f344ff689d8e873a357c
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2022-08-25 18:32:15 +0300

    Create .numRescueBoats.py

---
+++ week12/.numRescueBoats.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right = 0, len(people) - 1
+        count = 0
+        while left <= right:
+            if people[left] + people[right] > limit:
+                count += 1
+                right -= 1
+            else:
+                count += 1
+                right -= 1
+                left += 1
+        return count
+        
+        
