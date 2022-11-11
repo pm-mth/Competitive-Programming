512d4e90396b69b2fdd19e41090ee9523237a2f0
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2022-11-11 22:52:44 +0300

    Create .removeDuplicatesInt.py

---
+++ week15/.removeDuplicatesInt.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        L = 1
+        for R in range(1, len(nums)):
+            if nums[R-1] != nums[R]:
+                nums[L] = nums[R]
+                L += 1       
+        return L
