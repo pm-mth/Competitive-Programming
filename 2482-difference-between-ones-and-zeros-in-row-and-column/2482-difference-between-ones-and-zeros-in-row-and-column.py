13a249822b27a947b59fc219d7f44964c9ad65de
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2022-12-28 21:33:50 +0300

    Time: 2307 ms (73.48%), Space: 72.8 MB (5.11%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -9,13 +9,13 @@ class Solution:
                 row_count[(r, grid[r][c])] += 1
                 col_count[(c, grid[r][c])] += 1
         
-        diff = [[0 for _ in range(cols)] for _ in range(rows)]
+        # diff = [[0 for _ in range(cols)] for _ in range(rows)]
         
         for r in range(rows):
             for c in range(cols):
-                diff[r][c] = (row_count[(r, 1)] +  
+                grid[r][c] = (row_count[(r, 1)] +  
                               col_count[(c, 1)] - 
                               row_count[(r, 0)] - 
                               col_count[(c, 0)])
-        return diff
+        return grid
                 
