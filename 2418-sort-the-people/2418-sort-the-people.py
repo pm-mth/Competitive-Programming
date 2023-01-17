d3adbe0fed391e2ed7833daaded2b702148ebef5
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2023-01-17 19:22:42 +0300

    Time: 580 ms (10.84%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,19 +1,31 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
-        index = 0
-        while index < len(names):
-            swap = 0
-            for j in range(len(heights)-1):
-                if heights[j] < heights[j+1]:
-                    heights[j], heights[j+1] = heights[j+1], heights[j]
-                    names[j],  names[j+1] = names[j+1], names[j]
-                    swap += 1
-            if swap == 0:
-                index = len(names)
-            
-            index += 1
-        return names
-                    
-            
+      
+        #bubble Sort
         
+#         index = 0
+#         while index < len(names):
+#             swap = 0
+#             for j in range(index, len(heights)-1):
+#                 if heights[j] < heights[j+1]:
+#                     heights[j], heights[j+1] = heights[j+1], heights[j]
+#                     names[j],  names[j+1] = names[j+1], names[j]
+#                     swap += 1
+#             if swap == 0:
+#                 index = len(names)
+            
+#             index += 1
+#         return names
+        
+    #selection Sort
+    
+        for i in range(len(names)):
+            curMin = i
+            for j in range(i, len(names)):
+                if heights[curMin] < heights[j]:
+                    curMin = j
+            heights[curMin], heights[i] = heights[i], heights[curMin]
+            names[curMin], names[i] = names[i], names[curMin]
+            
+        return names
