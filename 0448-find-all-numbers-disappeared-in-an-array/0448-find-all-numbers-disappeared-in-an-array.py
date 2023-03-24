16e0e1ed47b4f1b566487bffd1c78812868daa7f
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2023-03-24 17:42:44 +0300

    Time: 423 ms (11.97%), Space: 22.3 MB (60.59%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        i = 0
+        while i < len(nums):
+            if nums[i] == i + 1:
+                i += 1
+            elif nums[i] == nums[nums[i]- 1]:
+                i += 1
+            else:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i] - 1]
+        answer = []
+        for i in range(len(nums)):
+            if nums[i] !=  i + 1 :
+                answer.append(i + 1)
+        return answer
+            
