2c3d3e19ec025c059fff4d9ed4e3cb2ae389c0db
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2023-03-27 21:57:15 +0300

    Time: 4953 ms (6.07%), Space: 32.4 MB (94.33%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -1,7 +1,7 @@
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
         self.count = 0
-        self.diff = diff
+        self.diff   = diff
         nums = []
         for i in range(len(nums1)):
             nums.append(nums1[i] - nums2[i])
@@ -19,19 +19,11 @@ class Solution:
         mid = l + (r - l)//2
         left = self.divide(l, mid, nums)
         right = self.divide(mid, r, nums)
-        
         return self.conquer(left, right)
+    
     def conquer(self, left, right):
-        p1, p2 = 0, 0
-        acc = 0
-        while p1 < len(left) and p2 < len(right):
-            if left[p1] <= right[p2] + self.diff:
-                acc += 1
-                p1 += 1
-            else:
-                self.count += acc
-                p2 += 1
-        for _ in range(p2, len(right)):
+        for i in range(len(right)):
+            acc = self.binarySearch(-1, len(left), right[i], left)
             self.count += acc
         
         p1, p2 = 0, 0
@@ -47,6 +39,18 @@ class Solution:
         merge.extend(right[p2:])
         
         return merge
+    def binarySearch(self, l, r, pivot, left):
+        
+        
+        while l + 1 < r:
+            mid = l + (r - l)//2
+         
+            if left[mid] <= pivot + self.diff:
+                l = mid
+            else:
+                r = mid  
+     
+        return r
     
     
     
