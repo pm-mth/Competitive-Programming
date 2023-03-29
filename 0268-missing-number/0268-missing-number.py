de2dfad7306c60abaf85f00c09eba02ff63774f8
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2023-03-29 19:37:42 +0300

    Time: 138 ms (59.22%), Space: 15.2 MB (66.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,18 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        i = 0
-        nums.append(float("inf"))
-        while i < len(nums):
-            if nums[i] == float("inf") or nums[i] == i:
-                i += 1
-            else:
-                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
-        for i in range(len(nums)):
-            if nums[i] == float("inf"):
-                return i
-                
+        #cyclic sort
+        res = 0
+        n = len(nums)
+        for i in range(n + 1):
+            res ^= i
         
+        for i in range(n):
+            res ^= nums[i]
         
+        return res
+    
+
         
         
        
