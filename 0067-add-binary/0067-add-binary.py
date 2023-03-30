ae9a94a75c071f259058b6afbe3880365d7fd9c5
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2023-03-30 19:21:42 +0300

    Time: 43 ms (16.14%), Space: 14 MB (18.09%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,39 +1,40 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        new_a = a[::-1]
-        new_b = b[::-1]
+        
         overflow = 0
         ans = []
-        i = 0
-        while i < len(new_a) and i < len(new_b):
-            s1, s2 = int(new_a[i]), int(new_b[i])
+        i = len(a) - 1
+        j = len(b) - 1
+        while i >= 0 and j >= 0:
+            s1, s2 = int(a[i]), int(b[j])
             temp = s1 ^ s2 ^ overflow
             ans.append(str(temp))
             if s1 & s2: 
                 overflow = 1
             elif s1 | s2 == 0:
                 overflow = 0
-            i += 1    
+            i -= 1
+            j -= 1
             
-        while i < len(new_a):
-            s1 = int(new_a[i])
+        while i >= 0:
+            s1 = int(a[i])
             temp = s1 ^ overflow
             if s1 & overflow:
                 overflow = 1
             elif s1 | overflow:
                 overflow = 0
             ans.append(str(temp))
-            i += 1
+            i -= 1
             
-        while i < len(new_b):
-            s1 = int(new_b[i])
+        while j >= 0:
+            s1 = int(b[j])
             temp = s1 ^ overflow
             ans.append(str(temp))
             if s1 & overflow:
                 overflow = 1
             elif s1 | overflow:
                 overflow = 0
-            i += 1
+            j -= 1
         if overflow:
             ans.append(str(overflow))
         return "".join(ans[::-1])
