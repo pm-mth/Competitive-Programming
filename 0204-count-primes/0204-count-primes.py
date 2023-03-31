e3a63d6e365e1e68c6895471d48c366df23f4ba2
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2023-03-31 19:35:32 +0300

    Time: 8300 ms (6.39%), Space: 67.6 MB (42.40%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -15,11 +15,8 @@ class Solution:
                 i += 2
             else:
                 i += 1
-        count = 0
-        for i in range(len(is_prime)):
-            if is_prime[i] == True:
-                count += 1
-        return count       
+        
+        return is_prime.count(True)     
                 
             
         
