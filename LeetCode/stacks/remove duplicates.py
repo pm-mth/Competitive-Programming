8f26d8bb26183db867656dc0895cb290470d33ec
Merry terefe <49280983+pm-mth@users.noreply.github.com>
2023-10-04 09:23:09 +0300

    Create remove duplicates.py

---
+++ LeetCode/stacks/remove duplicates.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+        for ch in s:
+            if stack and stack[-1][0] == ch:
+                stack[-1][1] += 1
+            else:
+                stack.append([ch, 1])
+            if stack and stack[-1][1] == k:
+                stack.pop()
+        
+        ans = ""
+        for ch, t in stack:
+            ans += ch * t
+        return ans
+
+            
+
+        
